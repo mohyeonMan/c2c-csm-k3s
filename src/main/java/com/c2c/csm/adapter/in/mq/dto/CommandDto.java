e908e572f4fddb6da180dc48a4d080991303f408
mohyeonMan <qkrwlgns0510@gmail.com>
2026-01-27 22:38:42 +0900

    fix : AckDto, CommandDto Setter 추가.

--- src/main/java/com/c2c/csm/adapter/in/mq/dto/CommandDto.java
+++ src/main/java/com/c2c/csm/adapter/in/mq/dto/CommandDto.java
@@ -1,9 +1,11 @@
 package com.c2c.csm.adapter.in.mq.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @ToString
 public class CommandDto {
     private String commandId;
